a7e6f9ec61773a19739d7d0472010aeefb0c05cd
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-01-18 11:24:14 -0500

    creamos el modal

--- src/components/rulesModal/index.js
+++ src/components/rulesModal/index.js
@@ -1,23 +1,26 @@
-import { createPortal } from "react-dom"
-import styles from "./styles.module.css"
+import { createPortal } from "react-dom";
+import styles from "./styles.module.css";
 
-function Modal({handleCloseModal}) {
-  
-  return createPortal (
-    <div className={styles.containerModal} onClick={handleCloseModal}>
-      <div className={styles.modal}>
-        <header className={styles.modalHeader}>
-          <h2>RULES</h2>
-          <button onClick={handleCloseModal}>
-            <img src="./images/icon-close.svg" alt="imagen"/>
-          </button>
-        </header>
-        <div className={styles.modalBody}>
-          <img src="./images/image-rules.svg" alt="imagens"/>
-        </div>
+function Modal({ handleCloseModal }) {
+  return(
+  <div className={styles.containerModal} onClick={handleCloseModal}>
+    <div className={styles.modal}>
+      <header className={styles.modalHeader}>
+        <h2>RULES</h2>
+        <button onClick={handleCloseModal}>
+          <img src="./images/icon-close.svg" alt="imagen" />
+        </button>
+      </header>
+      <div className={styles.modalBody}>
+        <img src="./images/image-rules.svg" alt="imagens" />
       </div>
     </div>
-  , document.getElementById("portal"))
+  </div>
+  )
 }
 
-export default Modal
+function RulesModal({handleCloseModal}) {
+  return createPortal(<Modal handleCloseModal={handleCloseModal}/>, document.getElementById("portal")) ;
+}
+
+export default RulesModal;
